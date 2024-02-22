fbf0f9210968ee8205837973260c1d242eb01e0f
Rex Hart <75512757+rexapcs@users.noreply.github.com>
2024-02-22 16:27:06 +0100

    TileMap Created

---
+++ Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("megaman_sprites_0"); // The player
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(0, player.transform.position.y +2, player.transform.position.z - 10);
+    }
+}
