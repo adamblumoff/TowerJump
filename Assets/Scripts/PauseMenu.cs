64027bb40eb87a24625dafa5c0a1fa524f3a2d8b
tagunloye <tagunloye@bowdoin.edu>
2024-02-26 16:16:22 +0100

    new changes

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] public GameObject PauseMenuPanel;
+    bool paused = false;
+    public string pauseMenuSceneName = "PauseMenu";
+
+    public void PausedGame()
+    {
+        if (paused)
+        {
+            Time.timeScale = 1;
+            paused = false;
+
+            SceneManager.UnloadSceneAsync(pauseMenuSceneName);
+        }
+        else
+        {
+            Time.timeScale = 0;
+            paused = true;
+            SceneManager.LoadScene(pauseMenuSceneName, LoadSceneMode.Additive);
+        }
+    }
+
+        public void Resume()
+    {
+        PauseMenuPanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("GameScene");
+    }
+}
