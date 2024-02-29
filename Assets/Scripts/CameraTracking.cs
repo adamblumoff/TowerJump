3479e7f7c8bf4b223e5b3aa46b86ff064c6c6501
Rex Hart <75512757+rexapcs@users.noreply.github.com>
2024-02-29 15:06:05 +0100

    reset controller

--- Assets/Scripts/CameraTracking.cs
+++ Assets/Scripts/CameraTracking.cs
@@ -5,7 +5,6 @@ public class CameraTracking : MonoBehaviour
 {
     public GameObject player;
     private CharacterController2D controllerInstance;
-    private bool isCharacterDead = false;
     public float smoothTime = .3F; // Smoothness of the camera movement
     private Vector3 currentVelocity = Vector3.zero; // Used by SmoothDamp
     private float previousYPosition;
@@ -22,28 +21,20 @@ public class CameraTracking : MonoBehaviour
     void Update()
     {
        
-        if (controllerInstance != null)
-        {
-            isCharacterDead = controllerInstance.AccessDead();
-            Debug.Log(isCharacterDead);
-        }
+       
 
         // Proceed to adjust the camera's position only if the character is not dead
-        if (!isCharacterDead)
-        {
+       previousYPosition = transform.position.y;
+       float targetYPosition = Mathf.Max(player.transform.position.y, transform.position.y);
+       if(player.transform.position.y - targetYPosition < -5)
+       {
+            ReloadScene();
+       }
+       // Determine the target position for the camera. Only Y is updated for rising effect
+       Vector3 targetPosition = new Vector3(transform.position.x, targetYPosition, transform.position.z);
 
-            previousYPosition = transform.position.y;
-            float targetYPosition = Mathf.Max(player.transform.position.y, transform.position.y);
-            if(player.transform.position.y - targetYPosition < -5)
-            {
-                ReloadScene();
-            }
-            // Determine the target position for the camera. Only Y is updated for rising effect
-            Vector3 targetPosition = new Vector3(transform.position.x, targetYPosition, transform.position.z);
-
-            // Smoothly transition to the target position
-            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime);
-        }
+       // Smoothly transition to the target position
+       transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime);
     }
 
     void ReloadScene()
--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -6,124 +6,156 @@ using UnityEngine.SceneManagement;
 
 public class CharacterController2D : MonoBehaviour
 {
-	[SerializeField] private float JumpForce = 300f;							// Amount of force added when the player jumps.
-	[Range(0, .3f)] [SerializeField] private float MovementSmoothing = .05f;	// How much to smooth out the movement
-	[SerializeField] private bool AirControl = false;							// Whether or not a player can steer while jumping;
-	[SerializeField] private LayerMask WhatIsGround;							// A mask determining what is ground to the character
-	[SerializeField] private Transform GroundCheck;							// A position marking where to check if the player is grounded.
-	[SerializeField] private Transform CeilingCheck;							// A position marking where to check for ceilings
-	public float health = 100f;
-
-	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-	private bool Grounded;            // Whether or not the player is grounded.
-	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
-	private Rigidbody2D Rigidbody2D;
-	private bool FacingRight = true;  // For determining which way the player is currently facing.
-	private Vector3 velocity = Vector3.zero;
-	private Animator playerAnimiator;
-	private bool dead = false;
+    [SerializeField] private float JumpForce = 300f;                            // Amount of force added when the player jumps.
+    [Range(0, .3f)][SerializeField] private float MovementSmoothing = .05f; // How much to smooth out the movement
+    [SerializeField] private bool AirControl = false;                           // Whether or not a player can steer while jumping;
+    [SerializeField] private LayerMask WhatIsGround;                            // A mask determining what is ground to the character
+    [SerializeField] private Transform GroundCheck;                         // A position marking where to check if the player is grounded.
+    [SerializeField] private Transform CeilingCheck;                            // A position marking where to check for ceilings
+    public float health = 100f;
+    public float airFactor;
 
 
-	[Header("Events")]
-	[Space]
-
-	public UnityEvent OnLandEvent;
-
-	[System.Serializable]
-	public class BoolEvent : UnityEvent<bool> { }
-
-
-	void Awake()
-	{
-		Rigidbody2D = GetComponent<Rigidbody2D>();
-
-		if (OnLandEvent == null)
-			OnLandEvent = new UnityEvent();
-
-		playerAnimiator = GetComponent<Animator>();
-	}
-
-	void Update()
-	{
-		bool wasGrounded = Grounded;
-		Grounded = false;
-
-		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
-		Collider2D[] colliders = Physics2D.OverlapCircleAll(GroundCheck.position, k_GroundedRadius, WhatIsGround);
-		for (int i = 0; i < colliders.Length; i++)
-		{
-			if (colliders[i].gameObject != gameObject)
-			{
-				Grounded = true;
-				if (!wasGrounded)
-					OnLandEvent.Invoke();
-			}
-		}
-		
-		if(dead){
-			DieAnimation();
-			dead = false;
-		}
-	}
+    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    private bool Grounded;            // Whether or not the player is grounded.
+    const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
+    private Rigidbody2D Rigidbody2D;
+    private bool FacingRight = true;  // For determining which way the player is currently facing.
+    private Vector3 stop = Vector3.zero;
+    private Animator playerAnimiator;
+    private bool dead = false;
+    private SpriteRenderer spriteRenderer;
 
 
-	public void Move(float move, bool jump, bool shooting)
-	{
+    [Header("Events")]
+    [Space]
 
-		//only control the player if grounded or airControl is turned on
-		if (Grounded || AirControl)
-		{
+    public UnityEvent OnLandEvent;
 
-			// Move the character by finding the target velocity
-			Vector3 targetVelocity = new Vector2(move * 10f, Rigidbody2D.velocity.y);
-			// And then smoothing it out and applying it to the character
-			Rigidbody2D.velocity = Vector3.SmoothDamp(Rigidbody2D.velocity, targetVelocity, ref velocity, MovementSmoothing);
-
-			// If the input is moving the player right and the player is facing left...
-			if (move > 0 && !FacingRight)
-			{
-				// ... flip the player.
-				Flip();
-			}
-			// Otherwise if the input is moving the player left and the player is facing right...
-			else if (move < 0 && FacingRight)
-			{
-				// ... flip the player.
-				Flip();
-			}
-		}
-		// If the player should jump...
-		if (Grounded && jump)
-		{
-			// Add a vertical force to the player.
-			Grounded = false;
-			Rigidbody2D.AddForce(new Vector2(0f, JumpForce));
-		}
-		
-	}
+    [System.Serializable]
+    public class BoolEvent : UnityEvent<bool> { }
 
 
-	private void Flip()
-	{
-		// Switch the way the player is labelled as facing.
-		FacingRight = !FacingRight;
+    void Awake()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
 
-		transform.Rotate(0f,180f,0f);
-	}
-	public void TakeDamage (int damage)
-	{
-		health -= damage;
+        if (OnLandEvent == null)
+            OnLandEvent = new UnityEvent();
 
-		if (health <= 0)
-		{
-			dead = true;
-		}
-	}
-	private void DieAnimation()
-	{
-		playerAnimiator.SetBool("isDead", true);
-	}
+        playerAnimiator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        bool wasGrounded = Grounded;
+        Grounded = false;
+
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(GroundCheck.position, k_GroundedRadius, WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                Grounded = true;
+                if (!wasGrounded)
+                    OnLandEvent.Invoke();
+            }
+        }
+
+        if (dead)
+        {
+            DieAnimation();
+            dead = false;
+        }
+    }
+
+
+    public void Move(float move, bool jump, bool shooting)
+    {
+
+        //only control the player if grounded or airControl is turned on
+        if (Grounded || AirControl)
+        {
+
+            // Move the character by finding the target velocity
+            Vector3 targetVelocity = new Vector2(move * 10f, Rigidbody2D.velocity.y);
+            // And then smoothing it out and applying it to the character
+            Rigidbody2D.velocity = Vector3.SmoothDamp(Rigidbody2D.velocity, targetVelocity, ref stop, MovementSmoothing);
+
+            // If the input is moving the player right and the player is facing left...
+            if (move > 0 && !FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+            // Otherwise if the input is moving the player left and the player is facing right...
+            else if (move < 0 && FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+        }
+        // If the player should jump...
+        if (Grounded && jump)
+        {
+            // Add a vertical force to the player.
+            Grounded = false;
+            Rigidbody2D.AddForce(new Vector2(0f, JumpForce));
+        }
+        if (!Grounded)
+        {
+            // Move the character by finding the target velocity
+            Vector3 targetVelocity = new Vector2(move * 10f, Rigidbody2D.velocity.y);
+            // And then smoothing it out and applying it to the character
+            Rigidbody2D.velocity = Vector3.SmoothDamp(Rigidbody2D.velocity, targetVelocity, ref stop, MovementSmoothing) * airFactor;
+
+            // If the input is moving the player right and the player is facing left...
+            if (move > 0 && !FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+            // Otherwise if the input is moving the player left and the player is facing right...
+            else if (move < 0 && FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+        }
+
+
+    }
+
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        if (FacingRight)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+            spriteRenderer.flipX = false;
+        FacingRight = !FacingRight;
+
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            dead = true;
+        }
+    }
+    private void DieAnimation()
+    {
+        Rigidbody2D.velocity = stop;
+        playerAnimiator.SetBool("isDead", true);
+    }
 
 	public void MegamanDie()
 	{
